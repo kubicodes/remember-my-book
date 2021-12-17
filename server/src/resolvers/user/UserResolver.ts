e91fd77ee715110609ac1085da2674177830fc25
kubicodes <kubilayercikti1@gmail.com>
2021-12-18 00:50:39 +0100

    added login mutation

--- server/src/resolvers/user/UserResolver.ts
+++ server/src/resolvers/user/UserResolver.ts
@@ -1,10 +1,14 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../../entities/User";
 import { FieldError } from "../common_types/error_response/FieldError";
 import { RegisterOptions } from "./types/RegisterOptions";
 import { UserResponse } from "./types/UserResponse";
 import { validateRegisterOptions } from "./utils/validateRegisterOptions";
 import bcrypt from "bcrypt";
+import { LoginOptions } from "./types/LoginOptions";
+import { CustomContext } from "../common_types/CustomContext";
+import { validateLoginOptions } from "./utils/validateLoginOptions";
+import { detectLoggedInBy } from "./utils/detectLoggedInBy";
 
 @Resolver(User)
 export class UserResolver {
@@ -42,6 +46,73 @@ export class UserResolver {
     } catch (error) {
       return { errors: [{ message: "Internal Server Error" }] };
     }
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("loginOptions") loginOptions: LoginOptions,
+    @Ctx() { req, res, redis }: CustomContext
+  ): Promise<UserResponse> {
+    const validateLoginOptionsResult = validateLoginOptions(loginOptions);
+
+    if (validateLoginOptionsResult !== true) {
+      const typeCastedValidationResult =
+        validateLoginOptionsResult as FieldError;
+
+      return {
+        errors: [
+          {
+            field: typeCastedValidationResult.field,
+            message: typeCastedValidationResult.message,
+          },
+        ],
+      };
+    }
+
+    let matchedUser: User | undefined;
+    try {
+      const loggedInBy = detectLoggedInBy(loginOptions);
+      matchedUser =
+        loggedInBy === "email"
+          ? await User.findOne({
+              where: { email: loginOptions.usernameOrEmail },
+            })
+          : await User.findOne({
+              where: { username: loginOptions.usernameOrEmail },
+            });
+    } catch (error) {
+      return {
+        errors: [{ message: "Invalid Login Data" }],
+      };
+    }
+
+    if (!matchedUser) {
+      return {
+        errors: [{ message: "Invalid Login Data" }],
+      };
+    }
+
+    let isPasswordCorrect: boolean;
+    try {
+      isPasswordCorrect = await bcrypt.compare(
+        loginOptions.password,
+        matchedUser.password
+      );
+    } catch (error) {
+      return {
+        errors: [{ message: "Invalid Login Data" }],
+      };
+    }
+
+    if (!isPasswordCorrect) {
+      return {
+        errors: [{ message: "Invalid Login Data" }],
+      };
+    }
+
+    req.session.userId = matchedUser.id;
+
+    return {user: matchedUser}
   }
 
   @Query(() => String)
