0dec36e96a70afd8583b93b07f9b8298a6a1f4d4
kubicodes <kubilayercikti1@gmail.com>
2021-12-17 17:34:16 +0100

    add user entity and migration

---
+++ server/src/entities/User.ts
@@ -0,0 +1,21 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+@ObjectType()
+export class User extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field(() => String)
+  @Column({ unique: true })
+  email!: string;
+
+  @Field(() => String)
+  @Column({ unique: true })
+  username!: string;
+
+  @Column()
+  password!: string;
+}
