06f7122489eb9ba6ac6a63859cbcf3369db7007d
Kubi <kubilayercikti1@gmail.com>
2022-12-03 12:42:16 +0100

    remove google books api limit from config

--- src/shared/application-config/application-config.schema.ts
+++ src/shared/application-config/application-config.schema.ts
@@ -2,9 +2,6 @@ import { Static, Type } from "@sinclair/typebox";
 
 export const ApplicationConfigSchema = Type.Object({
     port: Type.Number(),
-    googleBooks: Type.Object({
-        queryLimit: Type.Number(),
-    }),
 });
 
 export type ApplicationConfig = Static<typeof ApplicationConfigSchema>;
