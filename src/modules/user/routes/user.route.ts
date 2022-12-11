3804d59d2ef73b370342e5f60e53976c45bb5ad9
kubicodes <65826609+kubicodes@users.noreply.github.com>
2022-12-11 15:46:58 +0100

    Add Endpoint to query user by ID (#16)

--- src/modules/user/routes/user.route.ts
+++ src/modules/user/routes/user.route.ts
@@ -3,13 +3,17 @@ import { logger } from "../../../shared/logger/logger";
 import { ErrorResponse } from "../../../shared/schema/error-api-response.schema";
 import { PasswordService } from "../../auth/password.service";
 import { PrismaDBClient } from "../../database/services/database-client.service";
-import { UserAlreadyExistsError } from "../schemas/custom-errors.schema";
+import { UserAlreadyExistsError, UserNotFoundError } from "../schemas/custom-errors.schema";
 import { UserApiResponse } from "../schemas/user-api-response.schema";
 import { UserService } from "../services/user.service";
 
 interface CreateUserRequestBody {
     username: string;
     password: string;
+}
+
+interface FindUserByIdRequestParams {
+    id: string;
 }
 
 const router: Router = express.Router();
@@ -36,6 +40,34 @@ router.post("/", async (req: Request<unknown, unknown, CreateUserRequestBody>, r
 
         // when the error is from this type we want to expose the internal error as well, as it's well formatted
         const errorMessage = error instanceof UserAlreadyExistsError ? `Error while creating user. ${error.message}` : "Error while creating user.";
+
+        res.status(500);
+        res.send({
+            message: errorMessage,
+        });
+    }
+});
+
+router.get("/:id", async (req: Request<FindUserByIdRequestParams>, res: Response<UserApiResponse | ErrorResponse>) => {
+    const id = req.params.id;
+
+    logger.debug(`GET Request coming into /user/${id}`);
+
+    const dbClient = PrismaDBClient.getInstance();
+    const passwordService = new PasswordService(logger);
+
+    const userService = new UserService(dbClient, passwordService, logger);
+
+    try {
+        const matchingUser = await userService.findById(id);
+
+        res.status(200);
+        res.send({
+            message: "Successfully found matching user",
+            user: { id: matchingUser.id, username: matchingUser.username, createdAt: matchingUser.createdAt, updatedAt: matchingUser.updatedAt },
+        });
+    } catch (error) {
+        const errorMessage = error instanceof UserNotFoundError ? error.message : `Error while finding user with ID ${id}`;
 
         res.status(500);
         res.send({
