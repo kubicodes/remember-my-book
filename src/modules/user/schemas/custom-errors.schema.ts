966980d59f6d325dd4312bba16a045292b73f7d8
Kubi <kubilayercikti1@gmail.com>
2022-12-15 19:38:58 +0100

    Custom Errors while User Creation

--- src/modules/user/schemas/custom-errors.schema.ts
+++ src/modules/user/schemas/custom-errors.schema.ts
@@ -1,2 +1,8 @@
-export class UserAlreadyExistsError extends Error {}
-export class UserNotFoundError extends Error {}
+// doing this as we want to always check if it's a custom error before including the error
+// messages to the api response. It is easier to it with a single type check
+export class CustomError extends Error {}
+
+export class UserAlreadyExistsError extends CustomError {}
+export class UserNotFoundError extends CustomError {}
+export class InvalidUsername extends CustomError {}
+export class InvalidPassword extends CustomError {}
